b2de0fa165c2f8e5ef228703cfa40aca82c5b4c4
codeinvisible <lamasandesh400@gmail.com>
2023-04-15 12:39:56 +0545

    morgan added

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { dbConnection } from "./typeorm/typeorm.connection";
 import { authRoutes } from "./routes/auth/auth.routes";
+import morgan from "morgan";
 require("dotenv").config();
 const app = express();
 
@@ -11,9 +12,9 @@ dbConnection()
   .catch((err) => console.log(err));
 
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use("/api/auth", authRoutes);
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.status(404).json({
     status: 404,
     message: "Not found",
